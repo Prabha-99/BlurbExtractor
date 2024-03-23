82773f8d7ebd3d7aac38c8287757544dfa9c3651
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2024-03-23 19:10:15 +0530

    Initial Commit

---
+++ src/main/java/com/example/blurbscraper/BlurbScraperApplication.java
@@ -0,0 +1,13 @@
+package com.example.blurbscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlurbScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlurbScraperApplication.class, args);
+    }
+
+}
